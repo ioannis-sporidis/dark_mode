1c0fa22cd12a2e39e1d16ea2d8196924ddecfef1
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-08-18 22:32:44 -0400

    Fixed date using Moment.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,13 @@
 import moment from 'moment';
 
 const Article = ({ title, snippet, date, length }) => {
-  console.log(date);
+  const fixedDate = moment(date).format('MMMM Do YYYY');
 
   return (
     <article className='post'>
       <h2>{title}</h2>
       <div className='post-info'>
-        <span>date</span>
+        <span>{fixedDate}</span>
         <span>{length} min read</span>
       </div>
       <p>{snippet}</p>
